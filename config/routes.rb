e2056c440b5bfb055cbc831d80e439292442800c
Evaldas <e-crede@outlook.com>
2022-06-11 23:30:06 +0200

    login screen adj

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :articles
-  devise_for :users
+  devise_for :users, path: 'user',
+             path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret',
+                           confirmation: 'verification', unlock: 'unblock', registration: 'register',
+                           sign_up: 'sign_up' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get '/p/:slug', to: 'articles#show_by_slug', as: 'post'
   # Defines the root path route ("/")
