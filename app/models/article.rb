c1d3846c014355eaf7cc7883c3eb990904f85b85
Evaldas <e-crede@outlook.com>
2022-06-13 22:18:37 +0200

    slug uniqueness - case insensitive

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
   validates :title, :brief, :content_md, presence: true
-  validates :slug, uniqueness: true
+  validates :slug, uniqueness: { case_sensitive: false }
 end
